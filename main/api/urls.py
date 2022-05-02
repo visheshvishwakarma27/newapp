8687ef12e66d1ae19ff863c63caf02841ab5ddad
Vishesh <visheshvishwakarma2700@gmail.com>
2022-05-02 17:20:51 +0530

    first commit

---
+++ main/api/urls.py
@@ -0,0 +1,37 @@
+from .views import RegisterAPI
+from knox import views as knox_views
+from .views import ChangePasswordView
+from django.urls import path, include
+from .views import LoginAPI
+
+
+from rest_framework import permissions             # for swagger
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MySite API",
+      default_version='v5',
+      description="USER REGISTRATION API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="my license"),
+   )
+)
+
+
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/',knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/',knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')), 
+    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),   
+    
+]
---
+++ main/home/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from home.models import contact    #added manually
+admin.site.register(contact)
